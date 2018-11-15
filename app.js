4cdb0719974d9cf93545a68987f239baf2e102bd
vic3king <vic3coorp@gmail.com>
2018-11-15 12:26:54 +0000

    coonect to postgress and setup frontend design with bootstrap

--- app.js
+++ app.js
@@ -4,11 +4,15 @@ const path = require('path')
 const bodyParser = require('body-parser')
 const cons = require('consolidate')
 const dust = require('dustjs-helpers')
-const pg = require('pg')
+// const pg = require('pg')
 const app = express()
+const { Pool, Client } = require('pg')
 
 //db connect string
-const connect = "postgres://victory:2020ada@localhost/recipebookdb"
+const connectionString = "postgres://victory:2020ada@localhost/recipebookdb"
+
+//create pool for client
+const pool = new Pool({ connectionString })
 
 //assign dust engine to .dust files
 app.engine('dust', cons.dust)
@@ -25,7 +29,19 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 
 app.get('/', (req, res) => {
-  res.render('index')
+  //connect postgress
+  pool.connect((err, client, done) => {
+    if (err) {
+      return console.error('Error fetching client from pool', err)
+    }
+    client.query('SELECT * FROM recipes', (err, result) => {
+      if (err) {
+        console.error('Error running query', err)
+      }
+      res.render('index', { recipes: result.rows })
+      done()
+    })
+  })
 })
 
 //server 
